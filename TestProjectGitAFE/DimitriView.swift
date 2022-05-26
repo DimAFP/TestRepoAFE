b836056aa7bedc42a57565d5748f0602659d105c
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-05-26 08:08:28 +0200

    DimitriView

---
+++ TestProjectGitAFE/DimitriView.swift
@@ -0,0 +1,20 @@
+//
+//  DimitriView.swift
+//  TestProjectGitAFE
+//
+//  Created by dimitri on 26/05/2022.
+//
+
+import SwiftUI
+
+struct DimitriView: View {
+    var body: some View {
+        Text("Ecran Dimitri")
+    }
+}
+
+struct DimitriView_Previews: PreviewProvider {
+    static var previews: some View {
+        DimitriView()
+    }
+}
