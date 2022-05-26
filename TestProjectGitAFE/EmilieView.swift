9d5bba890cc0ce68c80b6b5d3cbbcbec6b55805d
Apprenant 08 <apprenant08@macbook-pro-de-apprenant.home>
2022-05-26 08:07:44 +0200

    EmilieView

---
+++ TestProjectGitAFE/EmilieView.swift
@@ -0,0 +1,20 @@
+//
+//  EmilieView.swift
+//  TestProjectGitAFE
+//
+//  Created by Apprenant 08 on 26/05/2022.
+//
+
+import SwiftUI
+
+struct EmilieView: View {
+    var body: some View {
+        Text("Ecran d'Emilie")
+    }
+}
+
+struct EmilieView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmilieView()
+    }
+}
