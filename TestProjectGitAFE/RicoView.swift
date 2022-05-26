4d28b726e3d508ebe59e1839149be8c1b1a8c717
Apprenant 08 <apprenant08@macbook-pro-de-apprenant.home>
2022-05-26 08:17:00 +0200

    RicoView

---
+++ TestProjectGitAFE/RicoView.swift
@@ -0,0 +1,20 @@
+//
+//  RicoView.swift
+//  TestProjectGitAFE
+//
+//  Created by Apprenant 08 on 26/05/2022.
+//
+
+import SwiftUI
+
+struct RicoView: View {
+    var body: some View {
+        Text("Ecran de Rico")
+    }
+}
+
+struct RicoView_Previews: PreviewProvider {
+    static var previews: some View {
+        RicoView()
+    }
+}
