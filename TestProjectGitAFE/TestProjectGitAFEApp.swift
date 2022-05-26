5fd7cc792cd74b882e165c11f72e2db3add8f91b
Apprenant 08 <apprenant08@macbook-pro-de-apprenant.home>
2022-05-26 07:51:04 +0200

    my first commit

---
+++ TestProjectGitAFE/TestProjectGitAFEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectGitAFEApp.swift
+//  TestProjectGitAFE
+//
+//  Created by Apprenant 08 on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectGitAFEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
